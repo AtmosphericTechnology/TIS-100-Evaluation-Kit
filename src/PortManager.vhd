cc7997d80b5f6125dd929904e2a1a56d15eaa3bd
AtmosphericSoftware <atmosphericsoftware@users.noreply.github.com>
2016-12-23 17:48:09 +1100

    Refactor PortManager.vhd. Edit PortManager.vhd
    Minor edits to the layout and structure of PortManager.vhd.
    Add some prerequisite logic to enable "any" and "last" to work correctly

--- src/PortManager.vhd
+++ src/PortManager.vhd
@@ -3,18 +3,19 @@
 -- Website: AtmosphericTechnology.com
 -- Email/Contact: atmospherictechnology@gmail.com
 -- 
--- Create Date:	 13:42:36 10/04/2016 
--- Project Name: TIS-100_CPU
--- Module Name:	 PortManager - Behavioral 
+-- Create Date: 12:48:34 12/20/2016
+-- Design Name: TIS-100_PortManager
+-- Module Name:	PortManager - Behavioral 
+-- Project Name: TIS-100_Evaluation_Kit
 -- Tool versions: ISE 14.7
 -- Description: 
--- 	The PortManager manages all the ext. communications and protocol
--- 	details.
---
+-- 	
+-- Dependencies: 
+-- 	
 -- Revision: 
--- Revision 0.9 - Prerelease version
+-- 	Revision 0.9 - Prerelease version
 -- Additional Comments: 
--- 	More details of communication protocol to come
+--
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -25,23 +26,25 @@ use ieee.numeric_std.all;
 --library unisim;
 --use unisim.vcomponents.all;
 
--- TODO: After recently checking the timing for the ports, It is different to what was originally thought
---			They don't have to idle if the reading is coming from a port that is already ready.
---			If that port executes at the same time, then it needs to delay 1 cycle.
---			reading happens on the first cycle (if rxReq is ready) and the write always happens on the second cycle (it will delay 1 if there is no rx).
-
+-----------------------------------------------
+-- PortManager
+-- Description: 
+-----------------------------------------------
 entity PortManager is
 	port (clk : in std_logic;
 		rst : in std_logic;
-		commStart : in std_logic;
-		commPause : out std_logic;
-		commType : in std_logic_vector (1 downto 0);
-		rxPort : in std_logic_vector (2 downto 0);
-		txPort : in std_logic_vector (2 downto 0);
-		dataIn : in signed (10 downto 0);
-		dataOut : out signed (10 downto 0);
-		last : out std_logic_vector (2 downto 0);
+		clkEnable : in std_logic;
 		
+		commStart : in std_logic; -- Start the transfer
+		commPause : out std_logic; -- Pause the main core
+		commType : in std_logic_vector (1 downto 0); -- Type of transfer
+		rxPort : in std_logic_vector (2 downto 0); -- Port to receive from
+		txPort : in std_logic_vector (2 downto 0); -- Port to transmit to
+		dataIn : in std_logic_vector (10 downto 0); -- Data from core to transmit
+		dataOut : out std_logic_vector (10 downto 0); -- Data received to send to the core
+		debugLast : out std_logic_vector (2 downto 0); -- Debug port for last
+		
+		-- Rx Port I/O's
 		rxLeftOpen : out std_logic;
 		rxRightOpen : out std_logic;
 		rxUpOpen : out std_logic;
@@ -50,11 +53,12 @@ entity PortManager is
 		rxRightReq : in std_logic;
 		rxUpReq : in std_logic;
 		rxDownReq : in std_logic;
-		rxLeftData : in signed (10 downto 0);
-		rxRightData : in signed (10 downto 0);
-		rxUpData : in signed (10 downto 0);
-		rxDownData : in signed (10 downto 0);
+		rxLeftData : in std_logic_vector (10 downto 0);
+		rxRightData : in std_logic_vector (10 downto 0);
+		rxUpData : in std_logic_vector (10 downto 0);
+		rxDownData : in std_logic_vector (10 downto 0);
 		
+		-- Tx Port I/O's
 		txLeftOpen : in std_logic;
 		txRightOpen : in std_logic;
 		txUpOpen : in std_logic;
@@ -63,75 +67,71 @@ entity PortManager is
 		txRightReq : out std_logic;
 		txUpReq : out std_logic;
 		txDownReq : out std_logic;
-		txLeftData : out signed (10 downto 0);
-		txRightData : out signed (10 downto 0);
-		txUpData : out signed (10 downto 0);
-		txDownData : out signed (10 downto 0));
-
+		txLeftData : out std_logic_vector (10 downto 0);
+		txRightData : out std_logic_vector (10 downto 0);
+		txUpData : out std_logic_vector (10 downto 0);
+		txDownData : out std_logic_vector (10 downto 0));
 end PortManager;
 
 architecture Behavioral of PortManager is
+	-- Internal signals
 	signal rxOpen : std_logic := '0';
 	signal rxReq : std_logic := '0';
-	signal rxComplete : std_logic := '0';
-	signal rxData : signed (10 downto 0) := "00000000000";
-	signal rxDataReg : signed (10 downto 0) := "00000000000"; -- Internal register for rxData
-	signal rxDataNext : signed (10 downto 0) := "00000000000"; -- Holds next value for rxData, it the output
-	signal rxDataLoad : std_logic := '0'; -- Enabled when new data should be loaded (into the register)
+	signal rxReady : std_logic := '0'; -- Rx transfer is ready this cycle
+	signal rxData : std_logic_vector (10 downto 0) := (others => '0'); -- Data received from chosen port
+	
 	signal txOpen : std_logic := '0';
 	signal txReq : std_logic := '0';
-	signal txComplete : std_logic := '0';
-	signal txData : signed (10 downto 0) := "00000000000";
-	
-	-- lastInt = internal storage for last
-	signal lastInt : std_logic_vector (2 downto 0) := (others => '1'); -- set the 'last' register to an invalid port.
-	signal nextLast : std_logic_vector (2 downto 0) := lastInt;
+	signal txReady : std_logic := '0'; -- Tx transfer is ready this cycle
+	signal txData : std_logic_vector (10 downto 0) := (others => '0'); -- Data to transfer to chosen port
 
+	signal last : std_logic_vector (2 downto 0) := "111"; -- storage for the last register. stores the port of the last succesful transfer.
+	
+	-- Stores which port was actually used for the transfer
+	-- Needed for any and last to store correctly
+	signal rxPortEquiv : std_logic_vector (2 downto 0) := (others => '0');
+	signal txPortEquiv : std_logic_vector (2 downto 0) := (others => '0');
+	
+	-- State storage for the comm port finite state machine
 	type stateType is (S_IDLE, S_RX_TRANSFER, S_TX_TRANSFER);
 	signal state, nextState : stateType := S_IDLE;
 begin
 
-	dataOut <= rxDataNext; -- rxDataNext holds the current value for rxData. Needed because of the "sample and hold" style functionality
-	last <= lastInt;
+	dataOut <= rxData;
+	debugLast <= last;
 
-	-- TODO: Look into maybe using a variable instead of doing it this way
-	rxDataFFProc: process (clk, rxDataReg, rxDataLoad, rxData, rxDataNext) is
+	-------------------------------------
+	-- transferStatusBitProc
+	-- Description: Handles all the status bits related
+	-- 	to the communication protocols. handles 
+	-- 	which port "any" and "last" relate too and which port to direct
+	-- 	the different signals.
+	-------------------------------------
+	transferStatusBitProc: process(txData, rxPort, rxOpen, rxLeftReq, rxLeftData, rxRightReq, rxRightData, rxUpReq, rxUpData, rxDownReq, rxDownData, last, txLeftOpen, txReq, txRightOpen, txUpOpen, txDownOpen) is
 	begin
-		rxDataNext <= rxDataReg;
-		if rxDataLoad = '1' then
-			rxDataNext <= rxData;
-		end if;
-		
-		if rising_edge(clk) then
-			rxDataReg <= rxDataNext;
-		end if;
-	end process;
-
-	-- TODO: Change the ports to be busses
-	
-	-- put the selected port's data and status bits into the signals
-	rxTxDataStatusBitsProc: process (rxPort, rxOpen, txData, rxLeftReq, rxLeftData, rxRightReq, rxRightData, rxUpReq, rxUpData, rxDownReq, rxDownData, txLeftOpen, txReq, txRightOpen, txUpOpen, txDownOpen, txPort, rxReq, txOpen, lastInt) is
-		variable rxPortEffective : std_logic_vector (2 downto 0) := "111"; --  The effective port of rx (needed for last)
-		variable txPortEffective : std_logic_vector (2 downto 0) := "111"; --  The effective port of tx (needed for last)
-	begin
-		-- TODO: Break out some of the sections into their own processes, make it look neater
+		-- Set initial values for the signals
 		rxLeftOpen <= '0';
 		rxRightOpen <= '0';
 		rxUpOpen <= '0';
 		rxDownOpen <= '0';
 		rxReq <= '0';
-		rxData <= "00000000000";
+		rxReady <= '0';
+		rxData <= (others => '0');
+		rxPortEquiv <= rxPort;
 		
+		txOpen <= '0';
 		txLeftReq <= '0';
 		txRightReq <= '0';
 		txUpReq <= '0';
 		txDownReq <= '0';
+		txReady <= '0';
 		txLeftData <= txData;
 		txRightData <= txData;
 		txUpData <= txData;
 		txDownData <= txData;
-		txOpen <= '0';
+		txPortEquiv <= txPort;
 		
+		-- Route the chosen rx ports signals.
 		case rxPort is
 			when "010" => -- left
 				rxLeftOpen <= rxOpen;
@@ -149,8 +149,33 @@ begin
 				rxDownOpen <= rxOpen;
 				rxReq <= rxDownReq;
 				rxData <= rxDownData;
+			when "110" => -- any
+				-- Choose the port based on the precedence (left > right > up > down)
+				-- Rx does not make itself known until the tx does.
+				if (rxLeftReq = '1') then
+					rxLeftOpen <= rxOpen;
+					rxReq <= rxLeftReq;
+					rxData <= rxLeftData;
+					rxPortEquiv <= "010";
+				elsif (rxRightReq = '1') then
+					rxRightOpen <= rxOpen;
+					rxReq <= rxRightReq;
+					rxData <= rxRightData;
+					rxPortEquiv <= "011";
+				elsif (rxUpReq = '1') then
+					rxUpOpen <= rxOpen;
+					rxReq <= rxUpReq;
+					rxData <= rxUpData;
+					rxPortEquiv <= "100";
+				elsif (rxDownReq = '1') then
+					rxDownOpen <= rxOpen;
+					rxReq <= rxDownReq;
+					rxData <= rxDownData;
+					rxPortEquiv <= "101";
+				end if;
 			when "111" => -- last
-				case lastInt is
+				rxPortEquiv <= last;
+				case last is
 					when "010" => -- left
 						rxLeftOpen <= rxOpen;
 						rxReq <= rxLeftReq;
@@ -167,13 +192,15 @@ begin
 						rxDownOpen <= rxOpen;
 						rxReq <= rxDownReq;
 						rxData <= rxDownData;
-					when others => -- no comm yet, lock up the PortManager
+					when others =>
+						-- No transfer has happened yet, lock up the PortManager
 						null;
 				end case;
 			when others =>
 				null;
 		end case;
 		
+		-- Route the chosen tx ports signals.
 		case txPort is
 			when "010" => -- left
 				txOpen <= txLeftOpen;
@@ -187,8 +214,46 @@ begin
 			when "101" => -- down
 				txOpen <= txDownOpen;
 				txDownReq <= txReq;
+			when "110" => -- any
+				-- Tx makes itself known to all ports first then
+				-- Chooses the port based on its priority.
+				txLeftReq <= txReq;
+				txRightReq <= txReq;
+				txUpReq <= txReq;
+				txDownReq <= txReq;
+				
+				if (txLeftOpen = '1') then
+					txLeftReq <= txReq;
+					txRightReq <= '0';
+					txUpReq <= '0';
+					txDownReq <= '0';
+					txOpen <= txLeftOpen;
+					txPortEquiv <= "010";
+				elsif (txRightOpen = '1') then
+					txLeftReq <= '0';
+					txRightReq <= txReq;
+					txUpReq <= '0';
+					txDownReq <= '0';
+					txOpen <= txRightOpen;
+					txPortEquiv <= "011";
+				elsif (txUpOpen = '1') then
+					txLeftReq <= '0';
+					txRightReq <= '0';
+					txUpReq <= txReq;
+					txDownReq <= '0';
+					txOpen <= txUpOpen;
+					txPortEquiv <= "100";
+				elsif (txDownOpen = '1') then
+					txLeftReq <= '0';
+					txRightReq <= '0';
+					txUpReq <= '0';
+					txDownReq <= txReq;
+					txOpen <= txDownOpen;
+					txPortEquiv <= "101";
+				end if;
 			when "111" => -- last
-				case lastInt is
+				txPortEquiv <= last;
+				case last is
 					when "010" => -- left
 						txOpen <= txLeftOpen;
 						txLeftReq <= txReq;
@@ -201,36 +266,36 @@ begin
 					when "101" => -- down
 						txOpen <= txDownOpen;
 						txDownReq <= txReq;
-					when others => -- no comm yet, lock up the PortManager
+					when others =>
+						-- No transfer has happened yet, lock up the PortManager
 						null;
 				end case;
 			when others =>
 				null;
 		end case;
 		
-		if (rxOpen and rxReq) = '1' then
-			rxComplete <= '1';
-		else
-			rxComplete <= '0';
+		-- Signal if the transfer is ready to occur on this cycle
+		if ((rxOpen and rxReq) = '1') then
+			rxReady <= '1';
 		end if;
-		
-		if (txOpen and txReq) = '1' then
-			txComplete <= '1';
-		else
-			txComplete <= '0';
+		if ((txOpen and txReq) = '1') then
+			txReady <= '1';
 		end if;
 	end process;
 	
-	clkdProc: process(clk, rst) is
-		-- TODO: latch the txData and rxData so they hold their value.
+	---------------------------------------
+	-- fsmClkedProc
+	-- Description: Handles synchronous reset and changing of
+	-- 	states for the communication state machine.
+	---------------------------------------
+	fsmClkdProc: process(clk, rst) is
 	begin
 		if rising_edge(clk) then
 			if (rst = '1') then
 				state <= S_IDLE;
-				lastInt <= "111";
+				last <= "111";
 			else
-				state <= nextState;
-				lastInt <= nextLast;
+				state <= nextState; -- synchronous update of state machine's state
 				
 				if state = S_IDLE then
 					if nextState /= S_IDLE then
@@ -239,20 +304,22 @@ begin
 						end if;
 					end if;
 				elsif nextState = S_IDLE then
-					-- been too long, cant remember what should go here.
+					null; -- TODO: Add transfer completion code if needed
 				end if;
 			end if;
 		end if;
 	end process;
-
-	-- TODO: rx needs to happen on the first cycle, not the second cycle
-	outputProc: process(state, commStart, rxComplete, txComplete, commType, lastInt, rxPort, txPort) is
+	
+	---------------------------------------
+	-- fsmOutputProc
+	-- Description: 
+	---------------------------------------
+	fsmOutputProc: process(state, commStart, rxReady, commType, txReady) is
 	begin
 		rxOpen <= '0';
 		txReq <= '0';
-		rxDataLoad <= '0';
 		commPause <= '0';
-		nextLast <= lastInt;
+		
 		case state is
 			when S_IDLE =>
 				if commStart = '1' then
@@ -261,15 +328,12 @@ begin
 			when S_RX_TRANSFER =>
 				rxOpen <= '1';
 				commPause <= '1';
-				rxDataLoad <= '1';
-				nextLast <= rxPort; -- TODO: add support for last.
-				if (rxComplete = '1') and (commType = "00") then
+				if (rxReady = '1') and (commType = "00") then
 					commPause <= '0';
 				end if;
 			when S_TX_TRANSFER =>
 				txReq <= '1';
-				nextLast <= txPort;
-				if (txComplete = '0') then
+				if (txReady = '0') then
 					commPause <= '1';
 				else
 					commPause <= '0';
@@ -279,7 +343,7 @@ begin
 		end case;
 	end process;
 
-	nextStateProc: process(state, commStart, rxComplete, txComplete, commType) is
+	fsmNextStateProc: process(state, commStart, commType, rxReady, txReady) is
 	begin
 		nextState <= state;
 		case state is
@@ -292,7 +356,7 @@ begin
 					end if;
 				end if;
 			when S_RX_TRANSFER =>
-				if (rxComplete = '1') then
+				if (rxReady = '1') then
 					if (commType = "10") then
 						nextState <= S_TX_TRANSFER;
 					else
@@ -300,13 +364,11 @@ begin
 					end if;
 				end if;
 			when S_TX_TRANSFER =>
-				if (txComplete = '1') then
+				if (txReady = '1') then
 					nextState <= S_IDLE;
 				end if;
 			when others =>
 				nextState <= S_IDLE;
 		end case;
 	end process;
-
 end Behavioral;
-
